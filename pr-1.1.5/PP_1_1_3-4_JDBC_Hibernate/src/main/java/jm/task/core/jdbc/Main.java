4d75e2a6ea9b5d44f3acb18ac1daa6826a116251
Lansetnick <118694414+Lansetnick@users.noreply.github.com>
2023-07-06 15:57:20 +0500

    Add files via upload

---
+++ pr-1.1.5/PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,20 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        UserServiceImpl userServImpl = new UserServiceImpl();
+        userServImpl.createUsersTable();
+        for (int i = 0; i < 4; i++) {
+            String name = "Maksim";
+            userServImpl.saveUser(name, "Maksimovich", (byte) 3);
+            System.out.println("User с именем - " + name + " добавлен в базу данных");
+        }
+        System.out.println(userServImpl.getAllUsers());
+        userServImpl.cleanUsersTable();
+        userServImpl.dropUsersTable();
+    }
+}
